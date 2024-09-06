1be1fae9ffcb0925681117623917799538e4dcab
Steven B. <4163156+appatalks@users.noreply.github.com>
2024-09-05 21:11:12 -0500

    Update update_blacklist.sh

--- Debian/update_blacklist.sh
+++ Debian/update_blacklist.sh
@@ -6,9 +6,16 @@ mkdir -p /root/bind-blacklists/"$(date +"%d-%m-%Y")"
 cd /root/bind-blacklists/"$(date +"%d-%m-%Y")"
 # wget https://github.com/notracking/hosts-blocklists/raw/master/dnscrypt-proxy/dnscrypt-proxy.blacklist.txt
 wget https://raw.githubusercontent.com/ShadowWhisperer/BlockLists/master/RAW/Malware
+wget https://raw.githubusercontent.com/ShadowWhisperer/BlockLists/master/RAW/Remote
+wget https://raw.githubusercontent.com/ShadowWhisperer/BlockLists/master/RAW/Risk
+wget https://raw.githubusercontent.com/ShadowWhisperer/BlockLists/master/RAW/Scam
 cp /var/cache/bind/blacklist.db .
 cat blacklist.db | awk '{print $1}'| sort > blacklist.current
 # grep -v "#" dnscrypt-proxy.blacklist.txt >> blacklist.current
+cat Malware >> blacklist.current
+cat Remote >> blacklist.current
+cat Risk >> blacklist.current
+cat Scam >> blacklist.current
 cat blacklist.current | sort | uniq > newblacklist.zones
 for i in $(cat newblacklist.zones); do echo "$i CNAME ." >>  newblacklist.db; done
 systemctl stop named.service
